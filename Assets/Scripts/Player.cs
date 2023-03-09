41f60088e6544b1243a0b4fba20dcff0365f1459
krb3d <rumpl.zol@gmail.com>
2023-03-09 10:59:26 +0100

    Movable Character, 01:16:00

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace ZZOT.KitchenChaos
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float playesSpeed = 7f;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            Vector2 input = new();
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                input += Vector2.up; // y+1
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                input += Vector2.down; // y-1
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                input += Vector2.left; // x-1
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                input += Vector2.right; // x+1
+            }
+
+            input = playesSpeed * Time.deltaTime * input.normalized;
+
+            Vector3 move = new(
+                            x: input.x,
+                            y: 0f,
+                            z: input.y);
+
+
+            transform.position += move;
+        }
+    }
+}
